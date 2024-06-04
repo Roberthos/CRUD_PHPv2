25938ca35d940d779a569f3dcd3096d5ccf62949
Roberth <robertholiveira36@gmail.com>
2024-06-04 14:26:00 -0300

    adição dos botões de edição

--- conexao.php
+++ conexao.php
@@ -11,3 +11,8 @@ $dbname="geral";
    $texto
   </div>";
  }
+ function mostra_data($data_vista){
+   $data_vista=explode("-",$data_vista);
+$escreve=$data_vista[2]."/".$data_vista[1]."/".$data_vista[0];
+return $escreve;
+ }
